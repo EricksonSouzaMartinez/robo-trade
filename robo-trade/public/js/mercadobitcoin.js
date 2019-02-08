d64e79cba2373d0fb73ac4c8cadda029c46beca4
Ericksonsouzamartinez <ericksonppu@gmail.com>
2019-02-08 15:42:28 -0300

    Começo de um novo projeto

--- robo-trade/public/js/mercadobitcoin.js
+++ robo-trade/public/js/mercadobitcoin.js
@@ -6,7 +6,9 @@ $("#comecar").click(function(){
 	// $.get("https://www.mercadobitcoin.net/api/BCH/ticker/", quantidadeNegociada);
 	// $.get("https://www.mercadobitcoin.net/api/XRP/ticker/", MaiorPrecodeOferta);
 	// $.get("https://www.mercadobitcoin.net/api/ETH/ticker/", pegaDados);
-	},300);	
+	Analise();
+	},3000);
+	SyncDados();
 });
 //https://www.mercadobitcoin.net/api-doc/
 
@@ -25,20 +27,79 @@ function pegaDados(data) {
 	var last = (data.ticker['last']);
 	var buy = (data.ticker['buy']);
 	var sell = (data.ticker['sell']);
-	var date = (data.ticker['date']);
+	var date =  obterHora();
 	
-	maiorpreco.text(" " + high);
-	menorPreco.text(" " + low);
-	quantidadeNegociada.text(" " + vol);
-	precoUnitario.text(" " + last);
-	maiorPrecodeOferta.text(" " + buy);
-	menorPrecodeOferta.text(" " + sell);
-	time.text(" " + date);
+	maiorpreco.text(high);
+	menorPreco.text(low);
+	quantidadeNegociada.text(vol);
+	precoUnitario.text(last);
+	maiorPrecodeOferta.text(buy);
+	menorPrecodeOferta.text( sell);
+	time.text( date);
 
 	var convert = $(".date").text();
 	var timestampIn = convert*1000;
 
-	//console.log(valorReal.text(" " + sell));
+	
 }
 
-	
+function obterHora() {
+
+	var data = new Date();
+	var dia = data.getDate();
+	var mes = data.getMonth() + 1;
+	var ano = data.getFullYear();
+	var hora = data.getHours();
+	var minuto = data.getMinutes();
+
+	//console.log(mes);
+	var informacoes = (dia+"/"+mes+"/"+ano+" "+hora+":"+minuto);
+	return informacoes;
+}
+
+
+function SyncDados() {
+
+	setInterval(function(){
+	var maiorpreco = $(".high").text();
+	var menorPreco = $(".low").text();
+	var quantidadeNegociada = $(".vol").text();
+	var precoUnitario = $(".last").text();
+	var maiorPrecodeOferta = $(".buy").text();
+	var menorPrecodeOferta = $(".sell").text();
+	var time = $(".date").text();
+
+
+    var placar = [];   
+    var score = {
+	    high: maiorpreco,
+		low: menorPreco,
+		vol: quantidadeNegociada,
+		last: precoUnitario,
+		buy: maiorPrecodeOferta,
+		sell: menorPrecodeOferta,
+		date: time
+        }
+       placar.push(score);
+//console.log(valores);
+  var dados = {
+       placar: placar
+    }
+
+   console.log(dados);
+   $.post("http://localhost:3000/placar", dados, function () {
+   console.log("Salvou os Usuarios no servidor");
+      // $(".tooltip").tooltipster("open")
+     // }).fail(function(){
+   // $(".tooltip").tooltipster("open").tooltipster("content","Falha ao sincornizar");
+   });
+      },3500);  
+
+}
+
+function Analise() {
+	var precoUnitario = $(".last").text();
+	if (precoUnitario < 13298){
+		alert("compra");
+	}
+}
